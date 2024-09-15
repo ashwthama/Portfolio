a86de9d9b5f171effa9da430b25b632e5dabfce3
Ashwani Kumar <60794217+ashwthama@users.noreply.github.com>
2024-09-15 13:53:37 +0530

    updated code with random front img

--- AshwaniPortfolio/src/app/Component/home/home.component.ts
+++ AshwaniPortfolio/src/app/Component/home/home.component.ts
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  img: string = "";
   constructor() { }
 
   ngOnInit(): void {
+    const rand=Math.random()*10;
+    if(rand<5) {
+      this.img = "FrontImageDark";
+    }else{
+    this.img = "FrontImage";
+    }
   }
+  
+  
   
 
 }
